f83ecdc713cdf814311b352d88df95f9822f54d1
Ulises Lira <moredialiraulises13@gmail.com>
2023-06-09 16:59:09 -0600

    05 Operadores lógicos y booleanos

---
+++ operadores_2.py
@@ -0,0 +1,22 @@
+# Operadores booleanos
+
+# == (igualdad)
+print(1 == 1)
+# != (distinto)
+print(1 != 1)
+# > (Mayor que)
+print(1 > 1)
+# < (Menor que)
+print(1 < 1)
+
+# Operadores Lógicos
+
+# and
+print(True and True)
+print(True and False)
+# or
+print(True or True)
+print(True or False)
+# not
+print(not True)
+
