fe6129f63049009e264f5912382d4ed03f21310b
Ulises Lira <moredialiraulises13@gmail.com>
2023-06-09 16:51:58 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,21 @@
+# operadores matematicos
+# + - * %
+# / Divición float
+# // Divición entera
+# ** Potencia
+
+print(12 + 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+
+# Operadores para strings
+# + para concatenar
+# * para replicar
+
+print("------- " * 4)
+print("Hola " + "Mundo!")
+print("Hola " * 3)
