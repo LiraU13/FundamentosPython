5acd2ab3b7c5f87390c71110125e999b830b8691
Ulises Lira <moredialiraulises13@gmail.com>
2023-06-09 16:20:41 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Python es NO tipado
+
+name = "Ulises"
+last_name = "Lira"
+age = 19
+height = 1.63
+is_student = True
+groups = ["3C"]
+
+# function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
